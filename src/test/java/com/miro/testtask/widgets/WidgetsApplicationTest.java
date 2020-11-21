5cfef6736ad610472144d4cf13af362aed63700c
Caio Quirino <caioquirino@gmail.com>
2020-11-21 17:23:01 +0100

    Improve the coverage check and ignore the Main class from the coverage

--- src/test/java/com/miro/testtask/widgets/WidgetsApplicationTest.java
+++ src/test/java/com/miro/testtask/widgets/WidgetsApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WidgetsApplicationTests {
+class WidgetsApplicationTest {
 
   @Test
   void contextLoads() {
